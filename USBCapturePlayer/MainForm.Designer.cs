26715376db770b0a4b38c542320723837419c5a3
dbqt <david.tran@polymtl.ca>
2015-06-27 22:41:37 -0400

    VideoSourceSelector

--- USBCapturePlayer/MainForm.Designer.cs
+++ USBCapturePlayer/MainForm.Designer.cs
@@ -29,19 +29,18 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.MainPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.VideoSourceSelector = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.AudioSourceSelector = new System.Windows.Forms.ComboBox();
-            this.VolumeBar = new System.Windows.Forms.TrackBar();
             this.label3 = new System.Windows.Forms.Label();
+            this.VolumeBar = new System.Windows.Forms.TrackBar();
             this.ActiveButton = new System.Windows.Forms.Button();
-            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.MainPanel = new System.Windows.Forms.Panel();
             this.MainVideoPlayer = new AForge.Controls.VideoSourcePlayer();
             this.flowLayoutPanel1.SuspendLayout();
-            this.MainPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).BeginInit();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -56,18 +55,8 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(669, 53);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(818, 53);
             this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // MainPanel
-            // 
-            this.MainPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.MainPanel.Controls.Add(this.MainVideoPlayer);
-            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.MainPanel.Location = new System.Drawing.Point(0, 53);
-            this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(669, 319);
-            this.MainPanel.TabIndex = 1;
             // 
             // label1
             // 
@@ -86,14 +75,14 @@
             this.VideoSourceSelector.FormattingEnabled = true;
             this.VideoSourceSelector.Location = new System.Drawing.Point(83, 15);
             this.VideoSourceSelector.Name = "VideoSourceSelector";
-            this.VideoSourceSelector.Size = new System.Drawing.Size(121, 21);
+            this.VideoSourceSelector.Size = new System.Drawing.Size(189, 21);
             this.VideoSourceSelector.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(210, 19);
+            this.label2.Location = new System.Drawing.Point(278, 19);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 13);
             this.label2.TabIndex = 2;
@@ -104,54 +93,57 @@
             // 
             this.AudioSourceSelector.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.AudioSourceSelector.FormattingEnabled = true;
-            this.AudioSourceSelector.Location = new System.Drawing.Point(290, 15);
+            this.AudioSourceSelector.Location = new System.Drawing.Point(358, 15);
             this.AudioSourceSelector.Name = "AudioSourceSelector";
-            this.AudioSourceSelector.Size = new System.Drawing.Size(121, 21);
+            this.AudioSourceSelector.Size = new System.Drawing.Size(204, 21);
             this.AudioSourceSelector.TabIndex = 3;
-            // 
-            // VolumeBar
-            // 
-            this.VolumeBar.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.VolumeBar.Location = new System.Drawing.Point(468, 3);
-            this.VolumeBar.Name = "VolumeBar";
-            this.VolumeBar.Size = new System.Drawing.Size(104, 45);
-            this.VolumeBar.TabIndex = 4;
-            this.VolumeBar.TickStyle = System.Windows.Forms.TickStyle.Both;
-            this.VolumeBar.Value = 5;
             // 
             // label3
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(417, 19);
+            this.label3.Location = new System.Drawing.Point(568, 19);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Volume:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // VolumeBar
+            // 
+            this.VolumeBar.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.VolumeBar.Location = new System.Drawing.Point(619, 3);
+            this.VolumeBar.Name = "VolumeBar";
+            this.VolumeBar.Size = new System.Drawing.Size(104, 45);
+            this.VolumeBar.TabIndex = 4;
+            this.VolumeBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.VolumeBar.Value = 5;
+            // 
             // ActiveButton
             // 
             this.ActiveButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.ActiveButton.Location = new System.Drawing.Point(578, 14);
+            this.ActiveButton.Location = new System.Drawing.Point(729, 14);
             this.ActiveButton.Name = "ActiveButton";
             this.ActiveButton.Size = new System.Drawing.Size(75, 23);
             this.ActiveButton.TabIndex = 6;
             this.ActiveButton.Text = "Play";
             this.ActiveButton.UseVisualStyleBackColor = true;
             // 
-            // videoSourcePlayer1
+            // MainPanel
             // 
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 0);
-            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.TabIndex = 0;
-            this.videoSourcePlayer1.VideoSource = null;
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MainPanel.Controls.Add(this.MainVideoPlayer);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 53);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(818, 319);
+            this.MainPanel.TabIndex = 1;
             // 
             // MainVideoPlayer
             // 
             this.MainVideoPlayer.Location = new System.Drawing.Point(12, 6);
             this.MainVideoPlayer.Name = "MainVideoPlayer";
-            this.MainVideoPlayer.Size = new System.Drawing.Size(645, 301);
+            this.MainVideoPlayer.Size = new System.Drawing.Size(794, 301);
             this.MainVideoPlayer.TabIndex = 0;
             this.MainVideoPlayer.Text = "VideoSourcePlayer";
             this.MainVideoPlayer.VideoSource = null;
@@ -160,7 +152,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 372);
+            this.ClientSize = new System.Drawing.Size(818, 372);
             this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.flowLayoutPanel1);
             this.MinimumSize = new System.Drawing.Size(685, 410);
@@ -168,8 +160,8 @@
             this.Text = "USB Capture Player";
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
-            this.MainPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.VolumeBar)).EndInit();
+            this.MainPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -186,7 +178,6 @@
         private System.Windows.Forms.TrackBar VolumeBar;
         private System.Windows.Forms.Button ActiveButton;
         private AForge.Controls.VideoSourcePlayer MainVideoPlayer;
-        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
     }
 }
 
--- USBCapturePlayer/MainForm.cs
+++ USBCapturePlayer/MainForm.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
 
 namespace USBCapturePlayer
 {
     public partial class MainForm : Form
     {
+        #region Constructors
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeProperties();
+            InitializeSelectors();
         }
+
+        #endregion
+
+
+        #region Properties
+
+        private FilterInfoCollection VideoDevices { get; set; }
+
+        #endregion
+
+
+        #region Methods
+
+        private void InitializeProperties()
+        {
+            
+        }
+
+        private void InitializeSelectors()
+        {
+            try
+            {
+                // Initialize all video devices.
+                VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (VideoDevices.Count == 0)
+                {
+                    throw new ApplicationException();
+                }
+
+                // Add all devices to source selector.
+                VideoSourceSelector.Items.Clear();
+
+                foreach (FilterInfo device in VideoDevices)
+                {
+                    VideoSourceSelector.Items.Add(device.Name);
+                }
+
+                // Enable source selector.
+                VideoSourceSelector.SelectedIndex = 0;
+                VideoSourceSelector.Enabled = true;
+            }
+            catch (ApplicationException)
+            {
+                // There was a problem in loading video sources, disabling the source selector.
+                VideoSourceSelector.Enabled = false;
+            }
+        }
+
+
+        #endregion
+        
+        
+        #region Events
+
+
+
+        #endregion
     }
 }
